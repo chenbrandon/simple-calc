dcc5d23816b4131d09ad38bb018930f1271dae78
Brandon Chen <chenb777@uw.edu>
2017-03-29 23:12:39 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Brandon Chen on 3/29/17.
+//  Copyright © 2017 Brandon Chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
