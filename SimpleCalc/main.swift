aa18d6ce19bb7e376bd29fdd75061a1937e6381b
Brandon Chen <chenb777@uw.edu>
2017-03-30 12:39:06 -0700

    finish hw and all extra credit

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -9,12 +9,19 @@
 
 import Foundation
 
-//let response: String = readLine(strippingNewline: true)!
 
+/* Handles command line arguments
+ * Examples call:
+ * $ ./main.swift fact 13
+ * $ ./main.swift + 4 -2.39
+ * $ ./main.swift % -49.8 -3.98
+ * $ ./main.swift avg 99 99 99 99 -99 49 3 3.49
+ * $ ./main.swift count 99 99 99 99 -99 49 3 3.49
+ */
 var args:[String] = []
-var filename = ""
-var method = ""
-for argument in CommandLine.arguments {
+var filename = "" // the filename is the first argument
+var method = ""   // the method name should be the second argument
+for argument in CommandLine.arguments { // the remaining arguments
     if(filename == "") {
         filename = argument
     } else if(method == "") {
@@ -24,41 +31,114 @@ for argument in CommandLine.arguments {
     }
 }
 
-if(method == "+") {
-    print(Double(args[0])! + Double(args[1])! )
-} else if(method == "-") {
-    print(Double(args[0])! - Double(args[1])! )
-} else if(method == "*") {
-    print( Double(args[0])! * Double(args[1])! )
-} else if(method == "/") {
-    print( Double(args[0])! / Double(args[1])! )
-} else if(method == "%") {
-    print( Double(args[0])!.truncatingRemainder(dividingBy:Double(args[1])! ))
-} else if(method == "count") {
-    print("The count is ", args.count)
-} else if(method == "avg") {
-    var max: Double = 0;
-    for num in args {
-        max += Double(num)!
+// Calculator functions
+func add(a: Double, b: Double) {
+    print(a + b)
+}
+func sub(a: Double, b: Double) {
+    print(a - b)
+}
+func mult(a: Double, b: Double) {
+    print(a * b)
+}
+func div(a: Double, b: Double) {
+    print(a / b)
+}
+func mod(a: Double, b: Double) {
+    print(a.truncatingRemainder(dividingBy: b))
+}
+func count(a: [Any]) {
+    print(a.count)
+}
+func fact(n: Double) -> Double {
+    if(n < 0) {
+        return -1
     }
-    if(max == 0) {
-        print("0")
-    } else {
-        print(max / Double(args.count))
+    if(n == 0) {
+        return 1;
     }
-} else if(method == "fact") {
-    func factorial(n: Double) -> Double {
-        if(n < 0) {
-            return -1
-        }
-        if(n == 0) {
-            return 1;
-        }
-        return n * factorial(n: n-1);
+    return n * fact(n: n-1);
+}
+func avg(a: [String]) {
+    let c = Double(a.count)
+    var sum: Double = 0;
+    for n in a {
+        
+        sum += Double(n.trimmingCharacters(in: .whitespaces))!
     }
-    print(factorial(n: Double(args[0])!))
-} else {
-    print("Invalid") // shouldnt reach here
+    print(sum / c)
 }
 
 
+// Start here
+if(method != "") { // User invoke script with command arguments, ex: $ ./main.swift + -39 3.49
+    if(method == "+") {
+        add(a: Double(args[0])!, b: Double(args[1])! )
+    } else if(method == "-") {
+        sub(a: Double(args[0])!, b: Double(args[1])! )
+    } else if(method == "*") {
+        mult(a: Double(args[0])!, b: Double(args[1])! )
+    } else if(method == "/") {
+        div(a: Double(args[0])!, b: Double(args[1])! )
+    } else if(method == "%") {
+        mod(a: Double(args[0])!, b: Double(args[1])! )
+    } else if(method == "count") {
+        count(a: args)
+    } else if(method == "avg") {
+        avg(a: args)
+    } else if(method == "fact") {
+        print(fact(n: Double(args[0])!))
+    } else { // Offer help text to client
+        print("It looks like you are trying to call this program with command line arguments")
+        print("This feature requires that you enter in the arguments in a specific format")
+        print("Operation first (\"+\", \"-\",\"/\",\"*\",\"%\",\"fact\",\"avg\", \"count\")")
+        print("Followed by the number(s)")
+        print("Here are some examples")
+        print("$ ./main.swift fact 13")
+        print("$ ./main.swift + 4 -2.39")
+        print("$ ./main.swift % -49.8 -3.98")
+        print("$ ./main.swift avg 99 99 99 99 -99 49 3 3.49")
+        print("$ ./main.swift count 99 99 99 99 -99 49 3 3.49")
+    }
+} else { // user did not use command line arguments, prompt user
+    print("Enter an expression separated by returns")
+    let num1: String = readLine(strippingNewline: true)!
+    let res1: String = readLine(strippingNewline: true)!
+    if(res1 == "fact") {
+        print(fact(n: Double(num1)!))
+    } else if(res1 == "count") {
+        print(1)
+    } else if(res1 == "avg") {
+        print(Double(num1)!)
+    }else if(res1 == "+" || res1 == "-" || res1 == "/" || res1 == "%" || res1 == "*") {
+        let num2: String = readLine(strippingNewline: true)!
+        if(res1 == "+") {
+            add(a: Double(num1)!, b: Double(num2)!)
+        } else if(res1 == "-") {
+            sub(a: Double(num1)!, b: Double(num2)!)
+            
+        }else if(res1 == "/") {
+            div(a: Double(num1)!, b: Double(num2)!)
+            
+        }else if(res1 == "%") {
+            mod(a: Double(num1)!, b: Double(num2)!)
+            
+        }else if(res1 == "*") {
+            mult(a: Double(num1)!, b: Double(num2)!)
+        }
+    } else { // keep interrogating until operand
+        var nums: [String] = [num1, res1]
+        var next: String = ""
+        repeat {
+            next = readLine(strippingNewline: true)!
+            if(next != "count" && next != "avg") {
+                nums.append(next)
+            }
+        } while(next != "count" && next != "avg")
+        if(next == "count") {
+            count(a: nums)
+        } else {
+            avg(a: nums)
+        }
+    }
+}
